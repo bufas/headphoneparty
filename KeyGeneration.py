afd87720d9bb174019f8f222d5a096dc84bfa7d1
Mathias Bak <bufas7@gmail.com>
2013-05-01 11:56:07 +0200

    Add support for signing messages and verifying signatures

--- KeyGeneration.py
+++ KeyGeneration.py
@@ -3,7 +3,8 @@ from Crypto.PublicKey import RSA
 
 
 class Generator():
-    BIT_MASK = 0b0000111  # Write 1 on the positions you want to be 0 in the hash
+    BIT_MASK = 0b0000001  # Write 1 on the positions you want to be 0 in the hash
+    key = None
 
     def __init__(self):
         result = 1
@@ -17,8 +18,8 @@ class Generator():
 
             # Hash the public key
             h1 = SHA256.new()
-            h1.update(str(publicKey.n).encode('utf-8'))
-            h1.update(str(publicKey.e).encode('utf-8'))
+            h1.update(str(publicKey.n).encode())
+            h1.update(str(publicKey.e).encode())
 
             # Hash the hashed public key
             h2 = SHA256.new()
@@ -35,8 +36,51 @@ class Generator():
             print('Try ' + str(counter) + ', fist byte was ' + '{0:08b}'.format(result))
 
         # A valid key pair was found. The printed stuff is just for debugging
+        self.key = keyObj
+
         print()
         print('YAY, we solved the crypto puzzle after ' + str(counter) + ' tries.')
         print('The resulting hash was: ' + h2.hexdigest())
 
-g = Generator()
+    def verifyMessage(self, publicKey, message, signature):
+        """Verifies a message signature"""
+        messageHash = SHA256.new(message.encode()).digest()
+        return publicKey.verify(messageHash, signature)
+
+    def signMessage(self, message):
+        """Signs a message with the private key"""
+        messageHash = SHA256.new(message.encode()).digest()
+        return self.key.sign(messageHash, None)
+
+    def getPublicKey(self):
+        return self.key.publickey()
+
+# Test
+# print('expected output: True, True, True, False, False, True')
+# g = Generator()
+# key1 = RSA.generate(2048)
+# key1Public = key1.publickey()
+#
+# m1 = 'This is message 1'
+# m2 = 'Message 2 is here'
+# m3 = 'Yet another message'
+#
+# m1Hash = SHA256.new(m1.encode()).digest()
+# m2Hash = SHA256.new(m2.encode()).digest()
+#
+# m1Sig = key1.sign(m1Hash, None)
+# m2Sig = key1.sign(m2Hash, None)
+#
+# print(key1Public.verify(m1Hash, m1Sig))
+# print(g.verifyMessage(key1Public, m1, m1Sig))
+# print(g.verifyMessage(key1Public, m2, m2Sig))
+#
+# print(g.verifyMessage(key1Public, m1, m2Sig))
+# print(g.verifyMessage(key1Public, m2, m1Sig))
+#
+#
+# g1 = Generator()
+# g2 = Generator()
+# m3Sig = g1.signMessage(m3)
+# g1PubKey = g1.getPublicKey()
+# print(g2.verifyMessage(g1PubKey, m3, m3Sig))
